16855acef085da9bf0c190f254547d0424f75722
Vivek Yadav <vivekyadav00681@gmail.com>
2026-04-15 19:55:43 +0530

    initial commit

---
+++ src/main/java/com/example/zeronet/ZeronetApplication.java
@@ -0,0 +1,13 @@
+package com.example.zeronet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeronetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeronetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zeronet/ZeronetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zeronet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeronetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
